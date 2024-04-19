289125d15e78e8e1f46b9427f8942f2bfb5463b0
austin-aguilar <austin47a@gmail.com>
2024-04-19 14:31:07 -0400

    attempted routing, server not working

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,23 +1,31 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware 
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise
 
-from src.db.register import register_tortoise
-from src.db.config import Tortoise_ORM
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
 
-Tortoise.init_models(['src.db.models'], 'models')
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["src.database.models"], "models")
+
+from src.routes import users, notes
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8080"],
-    allow_credentials = True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(users.router)
+app.include_router(notes.router)
 
-register_tortoise(app, config=Tortoise_ORM, generate_schemas = False)
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
 
 @app.get("/")
 def home():
-    return "Hello World!"
+    return "Hello, World!"
